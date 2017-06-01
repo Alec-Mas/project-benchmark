fddc72bf1f645b0174cdd52904b266ba9bab220c
tim <tim>
2017-06-01 23:03:52 +1000

    coming soon

---
+++ resources/views/Benchmark.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app') @section('content')
+<section class="benefits" id="benefits-section">
+    <div class="hero">
+        <div class="container">
+            <div class="content">
+                <h1 class="page-title">Coming Soon!!!</h1>
+				<div class="feature-wrapper">
+				<input type="button" name="Enter Another Project" onclick="location.href='{{ url('') }}'" class="previous action-button" value="Enter Another Project" />
+				</div>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
