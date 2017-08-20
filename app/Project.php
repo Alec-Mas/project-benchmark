36110e5b19ce6dff63db37f65be232443cddc7e3
Alec <amaslij@deakin.edu.au>
2017-08-20 16:29:55 +1000

    Reinstated form validation.

--- app/Project.php
+++ app/Project.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-<<<<<<< HEAD
     protected $fillable = [
         'project_name', 'project_industry', 'project_size', 'project_start', 'project_end', 'project_actual_end', 'project_budget', 'project_actual_budget'
     ];
-=======
-    //
->>>>>>> 195650c8495aff77ffc9a7b06bc8841e81c01ba6
 }
--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
 
 Route::post('generate-report', 'ProjectController@AddProject');
-=======
-/*
-use Illuminate\Http\Request;
-
-Route::post('/Wizard', function(Request $request) {
-    $Project = new \App\Project;
-    $Project->name = $request->name;
-    $Project->industry = $request->industry;
-    $Project->teamSize = $request->size;
-	$Project->startDate = $request->startdate;
-    $Project->estEndDate = $request->enddate;
-    $Project->actEndDate = $request->actualdate;
-	$Project->budget = $request->start;
-    $Project->actBudget = $request->actual;
-    $Project->Email = $request->email;
-    $Project->save();
-    return redirect('/');
-});
-*/
->>>>>>> 195650c8495aff77ffc9a7b06bc8841e81c01ba6
