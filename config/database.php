697de6509a9e31bb90771bf25f583fdd5baa6435
unknown <timothyhinrichsen@gmail.com>
2017-10-04 23:57:31 +1100

    BR again

--- config/database.php
+++ config/database.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
-    //'default' => env('DB_CONNECTION', 'pgsql'),
+    //'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
